8548493e7156b3b5d8097525d3e27ce590c26d77
swiichy <boomheadshotmn@gmail.com>
2021-02-07 13:48:19 +0100

    .

--- JS/date.js
+++ JS/date.js
@@ -24,55 +24,6 @@ function getfreegeoipAPI() {
 
 getfreegeoipAPI();
 
-//http://worldtimeapi.org/api/ip to dynamically display the different information concerning the weather in the year
-
-/*let succes = function(data) {
-    console.log("donnees api", data);
-    //mobile
-    getcurrentZone = document.querySelector('.currentZone');
-    getDoty = document.querySelector('.doty');
-    getDotw = document.querySelector('.dotw');
-    getWk = document.querySelector('.wk');
-    //desktop-tablet
-    getcurrentZoneId = document.getElementById('currentZone');
-    getDotyId = document.getElementById('doty');
-    getDotwId = document.getElementById('dotw');
-    getWkId = document.getElementById('wk');
-    //console.log(data.datetime);
-    getcurrentZone.innerHTML = data.timezone;
-    getDoty.innerHTML = data.day_of_year;
-    getDotw.innerHTML = data.day_of_week;
-    getWk.innerHTML = data.week_number;
-    //
-    getcurrentZoneId.innerHTML = data.timezone;
-    getDotyId.innerHTML = data.day_of_year;
-    getDotwId.innerHTML = data.day_of_week;
-    getWkId.innerHTML = data.week_number;
-
-    if (getDotwId.innerHTML === "0") {
-        getDotwId.innerHTML = "7";
-    }
-
-    if(getDotw.innerHTML === "0") {
-        getDotw.innerHTML = "7";
-    }
-}
-
-function getworldtimeapiAPI() {
-    let url = "http://worldtimeapi.org/api/ip";
-
-    $.get(url, succes).done(function() {
-        //alert( "second success" );
-    })
-    .fail(function() {
-        alert( "error" );
-    })
-    .always(function() {
-        //alert( "finished" );
-    });
-}
-
-getworldtimeapiAPI();*/
 
 let getcurrentZone = document.querySelector('.currentZone'),
     getDoty = document.querySelector('.doty'),
@@ -88,10 +39,19 @@ fetch('http://worldtimeapi.org/api/ip')
     .then(res => {
         if(res.ok){
             res.json().then(data => {
+                //desktop
                 getcurrentZoneId.innerHTML = data.timezone;
+                getDotyId.innerHTML = data.day_of_year;
+                getDotwId.innerHTML = data.day_of_week;
+                getWkId.innerHTML = data.week_number;
+                //mobile
+                getcurrentZone.innerHTML = data.timezone;
+                getDoty.innerHTML = data.day_of_year;
+                getDotw.innerHTML = data.day_of_week;
+                getWk.innerHTML = data.week_number;
             })
         } else{
             console.log("ERROR");
         }
-    })
+    });
 
